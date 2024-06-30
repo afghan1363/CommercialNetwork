6dcbf656d37a3542cebd999a07da51c446e14a85
Maxim Abavi <maxim.abavi@ya.ru>
2024-06-30 22:52:45 +0500

    def create in user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -32,6 +32,19 @@ class NewUserSerializer(ModelSerializer):
 class NewUserWithOrgSerializer(ModelSerializer):
     organization = OrganizationSerializer()
 
+    def create(self, validated_data):
+        org = validated_data.pop('organization')
+        user = User.objects.create(
+            email=validated_data['email'],
+            phone=validated_data['phone'],
+            city=validated_data['city'],
+            moderator=validated_data['moderator']
+        )
+        
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 
     class Meta:
         model = User
